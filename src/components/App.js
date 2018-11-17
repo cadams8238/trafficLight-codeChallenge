9c7ddae363961563b08aa78ba7d290f516a2a04d
Courtney Adams <cadams8238@gmail.com>
2018-11-17 11:39:44 -0800

    styled container in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import styles from "../styles/App.module.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        hello world
-      </div>
-    );
-  }
+	render() {
+		return <div className={styles.container} />;
+	}
+}
+
+function fetchLight() {
+	return new Promise((resolve, reject) => {
+		switch (Math.floor(Math.random() * 3)) {
+			case 0:
+				resolve("red");
+			case 1:
+				resolve("yellow");
+			case 2:
+				resolve("green");
+		}
+	});
 }
 
 export default App;
